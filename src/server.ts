00da5ed494a08ebc7c39b09c1adbb26ad0afa413
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-04-20 23:44:41 +0200

    add routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import express from "express";
 import "./database";
 
+import { routes } from "./routes";
+
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Olá, NLW 05" });
-});
+app.use(express.json());
 
-app.post("/users", (request, response) => {
-    return response.json({ message: "usuário salvo com sucesso" });
-});
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log("Server in running on port:", PORT);
